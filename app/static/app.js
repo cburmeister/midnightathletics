e3990888a268298a1ff330ea946ac2b3c909fdd5
Corey Burmeister <burmeister.corey@gmail.com>
2018-05-12 13:27:05 -0700

    Include all streams in listeners count

--- app/static/app.js
+++ app/static/app.js
@@ -1,8 +1,11 @@
 function poll_icecast_status() {
-    $.getJSON("http://midnightathletics.com:8000/status-json.xsl", function(json){
-        var source = json.icestats.source[0];
-        $("#now-playing").text("Now playing: " + source.title);
-        $("#num-listeners").text("Listeners: " + source.listeners);
+    $.getJSON("http://midnightathletics.com:8000/status-json.xsl", function(json) {
+        var listeners = 0;
+        $.each(json.icestats.source, function(i, source){
+            listeners = listeners + source.listeners;
+        });
+        $("#now-playing").text("Now playing: " + json.icestats.source[0].title);
+        $("#num-listeners").text("Listeners: " + listeners);
     });
 }
 setInterval(poll_icecast_status, 5000);
