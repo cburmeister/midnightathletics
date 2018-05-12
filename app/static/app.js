85d0ada2b58834ed641945e66afe2b6ec7f9ed61
Corey Burmeister <burmeister.corey@gmail.com>
2018-05-12 13:17:34 -0700

    Shift now playing munging to the server

--- app/static/app.js
+++ app/static/app.js
@@ -1,15 +1,7 @@
 function poll_icecast_status() {
     $.getJSON("http://midnightathletics.com:8000/status-json.xsl", function(json){
         var source = json.icestats.source[0];
-        var title = source.title;
-        now_playing = title
-            .substring(title.lastIndexOf("/") + 1)
-            .replace(/\.[^/.]+$/, "")
-            .replace(/\./g, " ")
-            .split(" ")
-            .map(function(v){return v.charAt(0).toUpperCase() + v.slice(1)})
-            .join(" ");
-        $("#now-playing").text("Now playing: " + now_playing);
+        $("#now-playing").text("Now playing: " + source.title);
         $("#num-listeners").text("Listeners: " + source.listeners);
     });
 }
