a08dc54d0221f963e75ecb601dbdbb2d8ffca306
Corey Burmeister <burmeister.corey@gmail.com>
2018-05-24 12:32:30 -0700

    Sync uploaded mixes back to s3

--- bin/sync-mixes.sh
+++ bin/sync-mixes.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-aws s3 sync s3://mixes-b2d2c7df84340c274011ecdffcca275d /mnt/volume-sfo2-01/mixes
+bucket=s3://mixes-b2d2c7df84340c274011ecdffcca275d
+mixes_path=/mnt/volume-sfo2-01/mixes
+
+aws s3 sync $bucket $mixes_path
+aws s3 sync $mixes_path $bucket
